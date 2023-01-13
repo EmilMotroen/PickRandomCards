89585725c09edb1ecbc5574947db8e24434001d9
Emil Motrøen <e.motroen@gmail.com>
2023-01-13 13:42:59 +0100

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,21 @@
+﻿using PickRandomCards;
+
+class PickRandomCard
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number of cards to pick: ");
+        string line = Console.ReadLine();
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+            }
+        } 
+        else
+        {
+            Console.WriteLine("Could not convert input");
+        }
+    }
+}
